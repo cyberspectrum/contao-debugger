698865c5912b8e91bea839a8af2184e94ee03e64
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-08-27 03:57:17 +0200

    Add template debugging by request of @frontendschlampe.

---
+++ src/CyberSpectrum/ContaoDebugger/DebugBar/DataCollector/TemplateInspectionCollector.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Contao Debugger
+ *
+ * Copyright (c) 2014 Christian Schiffler
+ *
+ * @package     ContaoDebugger
+ * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright   2014 CyberSpectrum
+ * @license     LGPL-3+
+ * @filesource
+ */
+
+namespace CyberSpectrum\ContaoDebugger\DebugBar\DataCollector;
+
+use CyberSpectrum\ContaoDebugger\DebugBar\DataFormatter\DataFormatter;
+use DebugBar\DataFormatter\DataFormatterInterface;
+
+/**
+ * Traces Template variables.
+ */
+class TemplateInspectionCollector extends \DebugBar\DataCollector\RequestDataCollector
+{
+	/**
+	 * The messages.
+	 *
+	 * @var array
+	 */
+	protected $messages = array();
+
+	/**
+	 * Returns the default data formater.
+	 *
+	 * @return DataFormatterInterface
+	 */
+	public function getDataFormatter()
+	{
+		if ($this->dataFormater === null)
+		{
+			$this->dataFormater = new DataFormatter();
+		}
+
+		return $this->dataFormater;
+	}
+
+	/**
+	 * Adds a template.
+	 *
+	 * @param \Template $template The message.
+	 *
+	 * @return void
+	 */
+	public function addTemplate($template)
+	{
+		$this->messages[] = array(
+			'message' => $this->getDataFormatter()->formatVar($template->getData()),
+			'label' => $template->getName(),
+			'time' => microtime(true)
+		);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function collect()
+	{
+		$data = array();
+		$seq  = array();
+		foreach ($this->messages as $i => $var)
+		{
+			$label = $var['label'];
+			if (!isset($seq[$label]))
+			{
+				$seq[$label] = 0;
+			}
+			else
+			{
+				$seq[$label]++;
+			}
+
+			//$label = $label .  ' ' . $seq[$label];
+			$label = $i . ' ' . $label;
+
+			$data[$label] = $var['message'];
+		}
+
+		return array(
+			'templates' => $data,
+			'nb_templates' => count($this->messages)
+		);
+	}
+
+	/**
+	 * Retrieve the name.
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'template-inspection';
+	}
+
+	/**
+	 * Retrieve the widget information.
+	 *
+	 * @return array
+	 */
+	public function getWidgets()
+	{
+		return array(
+			'template-inspection'       => array(
+				'icon'              => 'tags',
+				'widget'            => 'PhpDebugBar.Widgets.VariableListWidget',
+				'map'               => 'template-inspection.templates',
+				'default'           => '{}'
+			),
+			'template-inspection:badge' => array
+			(
+				'map'               => 'template-inspection.nb_templates',
+				'default'           => 0
+			)
+		);
+	}
+}
---
+++ src/CyberSpectrum/ContaoDebugger/Templates/TemplateDebugger.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Contao Debugger
+ *
+ * Copyright (c) 2014 Christian Schiffler
+ *
+ * @package     ContaoDebugger
+ * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright   2014 CyberSpectrum
+ * @license     LGPL-3+
+ * @filesource
+ */
+
+namespace CyberSpectrum\ContaoDebugger\Templates;
+
+use CyberSpectrum\ContaoDebugger\DebugBar\DataCollector\TemplateInspectionCollector;
+
+/**
+ * Traces Template variables.
+ */
+class TemplateDebugger
+{
+	/**
+	 * The collector to use.
+	 *
+	 * @var TemplateInspectionCollector
+	 */
+	protected static $collector;
+
+	/**
+	 * Attach to the collector and HOOKs.
+	 *
+	 * @param TemplateInspectionCollector $collector The collector.
+	 *
+	 * @return void
+	 */
+	public static function attach($collector)
+	{
+		self::$collector = $collector;
+		array_insert($GLOBALS['TL_HOOKS']['parseTemplate'], 0, array(array(__CLASS__, 'parseTemplate')));
+	}
+
+	/**
+	 * Adds a template.
+	 *
+	 * @param \Template $template The message.
+	 *
+	 * @return void
+	 */
+	public function parseTemplate(\Template $template)
+	{
+		self::$collector->addTemplate($template);
+	}
+}
